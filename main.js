d639aa301c96326a1cd7d2e71471af63a9c2d679
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2026-04-16 11:45:17 +0200

    implementato modal per iscrizione newsletter a comparsa automatica su pagina index

--- main.js
+++ main.js
@@ -6,3 +6,10 @@ var ready = (callback) => {
 ready(() => {
     document.querySelector(".header").style.height = window.innerHeight + "px";
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        var demoModal = new bootstrap.Modal(document.getElementById('demo-modal'));
+        demoModal.show();
+    }, 500);
+});
